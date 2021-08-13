9e8e782b5282aea1207e5dbd0a1934f50ae11be9
June <williamreji66@gmail.com>
2021-08-13 14:02:04 -0700

    id things

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
+#4717
 import discord
  
 Client = discord.Client()
+GUILD = 'R6S'
 
-@Client.event
-async def on_ready():
-	print(f'{Client.user} has connected to Discord!')
+class MyClient(discord.Client):
+	async def on_ready(self):
+		guild = discord.utils.get(self.guilds, name=GUILD)
+		print(
+			f'{self.user} is connected to the following guild:\n'
+			f'{guild.name}(id: {guild.id})'
+		)
+	
+	async def on_member_join(self,member):
+		await member.create_dm()
+		await member.dm_channel.send(f'Welcome {GUILD},{member.name}')
+	
+	async def on_message(self,message):
+		if message.author == Client.user:
+			return
+		elif message.content.startswith('$hello'):
+			await message.channel.send('Hello :eyes:')
+		elif message.content == "$help":
+			await message.channel.send('```Help Commands \n```')
+		elif message.content == "$ping":
+			member = message.author
+			await member.create_dm()
+			await member.dm_channel.send(f'Welcome {GUILD},{member.name}')
+			print(f'{member.id}')
 
-Client.run("ODc1ODAxMzg1NjAwNDMwMTIw.YRazmQ.EaRcqOZV-92xUgQnMKBp94zDV88")
+
+client = MyClient()
+client.run("ODc1ODAxMzg1NjAwNDMwMTIw.YRazmQ.EaRcqOZV-92xUgQnMKBp94zDV88")
