1a1efc39722dd583f936fdfa29c5058c4f8f5a11
June <williamreji66@gmail.com>
2021-08-13 17:06:04 -0700

    ODIN IS WITH US...!
    marcin helped me(@RedSkittleFox)

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-#4717
 import discord
  
 GUILD = 'R6S'
@@ -10,25 +9,27 @@ class MyClient(discord.Client):
 			f'{self.user} is connected to the following guild:\n'
 			f'{guild.name}(id: {guild.id})'
 		)
+		for guild in self.guilds:
+			for member in guild.members:
+				print(f'{member}')
 
 	async def on_member_join(self,member):
 		await member.create_dm()
 		await member.dm_channel.send(f'Welcome {GUILD},{member.name}')
 
 	
-	async def on_message(self,message):
-		if message.author == Client.user:
+	async def on_message(self, message):
+		if message.author == self.user:
 			return
 		elif message.content.startswith('$hello'):
 			await message.channel.send('Hello :eyes:')
 		elif message.content == "$help":
 			await message.channel.send('```Help Commands \n```')
 		elif message.content == "$ping":
-			member = message.author
-			await member.create_dm()
-			await member.dm_channel.send(f'Welcome {GUILD},{member.name}')
-			print(f'{member.id}')
+			guild = message.guild
+			await message.author.create_dm()
+			await message.author.dm_channel.send(f'Welcome to {GUILD},')
+			
 
-
-client = MyClient()
-client.run()
+client = MyClient(intents=discord.Intents.all())
+client.run('ODc1ODAxMzg1NjAwNDMwMTIw.YRazmQ.FQHaLsp4pJ4OikvV_OVZP83UpzI')
