c4ab43b267d2be23653afc2a74b4396a702454c4
June <williamreji66@gmail.com>
2021-08-15 14:06:12 -0700

    a bit of explorations on roles

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import discord
- 
-GUILD = 'R6S'
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+RANGE = os.getenv('RANGE')
+TOKEN = os.getenv('TOKEN')
+GUILD = os.getenv('GUILD')
+SYMBOL = os.getenv('SYMBOL')
 
 class MyClient(discord.Client):
 	async def on_ready(self):
@@ -9,28 +16,58 @@ class MyClient(discord.Client):
 			f'{self.user} is connected to the following guild:\n'
 			f'{guild.name}(id: {guild.id})'
 		)
-		await guild.create_role("Big pp guy")
+		#print(len("$Getrole"))
 
 	async def on_member_join(self,member):
 		await member.create_dm()
 		await member.dm_channel.send(f'Welcome {GUILD},{member.name}')
+		member.guild_permissions = Permissions.none
 
 	
 	async def on_message(self, message):
 		if message.author == self.user:
 			return
-		elif message.content == "$hello":
-			await message.channel.send('Hello :eyes:')
-		elif message.content == "Role":
-			if message.author.administrator:
-				message.author.add_roles("Big pp guy")
-		elif message.content == "$help":
-			await message.channel.send('```Help Commands \n```')
-		elif message.content == "$ping":
-			guild = message.guild
-			await message.author.create_dm()
-			await message.author.dm_channel.send(f'boop {message.author.name}')
-			
+		else:
+			if message.content[0] == SYMBOL:
+				parameter = ''
+				for i in range(0,int(RANGE)):
+					parameter =parameter + message.content[i]
+					#print (parameter)
+					if i == 4:
+						if parameter == "$ping":
+							guild = message.guild
+							await message.author.create_dm()
+							await message.author.dm_channel.send(f'boop {message.author.name}')
+							break
+						elif parameter == "$help":
+							await message.channel.send('```Help Commands \n```')
+							break
+					if i == 5:
+						if parameter == "$hello":
+							await message.channel.send('Hello :eyes:')
+							break
+					if i == 7:
+						if parameter == "$Scalpel":
+							await message.channel.send("lub you doc..")
+							break
+						elif parameter == "$Getrole":
+							name = ''
+							for i in range(len('$Getrole')+1,len(message.content)):
+								name += message.content[i]
+							member = message.guild.get_member_named(name)
+							if member == None:
+								break
+							text = ''
+							for i in range(1,len(member.roles)):
+								text = member.roles[i].name + ''
+							await message.channel.send(f'{text}')
+							break
+						else:
+							await message.channel.send("ummm i don't know what to do")
+							break 
+					
+
+#Userroles - 9
 
 client = MyClient(intents=discord.Intents.all())
-client.run('ODc1ODAxMzg1NjAwNDMwMTIw.YRazmQ.zBOxd7qWuwceHKspbFmmn03C8dQ')
+client.run(TOKEN)
