617fb3fa37023ee7bae8cb17780074de85ee0e80
June <williamreji66@gmail.com>
2021-09-10 01:16:20 +0530

    bans

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ class MyClient(discord.Client):
 	async def on_member_join(self,member):
 		await member.create_dm()
 		await member.dm_channel.send(f'Welcome {GUILD},{member.name}')
-		member.guild_permissions = Permissions.none
+		member.guild_permissions.update()
 
 	async def on_reaction_add(self,reaction, user):
 		channel = reaction.message.channel
@@ -33,100 +33,126 @@ class MyClient(discord.Client):
 		if message.author == self.user:
 			return
 		else:
-			if message.content[0] == SYMBOL:
-				parameter = ''
-				for i in range(0,int(RANGE)):
-					parameter =parameter + message.content[i]
-					print (parameter)
-					if i == 4:
-						if parameter == "$ping":
-							guild = message.guild
-							await message.author.create_dm()
-							await message.author.dm_channel.send(f'boop {message.author.name}')
-							break
-						elif parameter == "$help":
-							await message.channel.send('```Help Commands \n```')
-							break
-					if i == 5:
-						if parameter == "$hello":
-							await message.channel.send('Hello :eyes:')
-							break
-					if i == 7:
-						if parameter == "$Scalpel":
-							await message.channel.send("lub you doc..")
-							break
-						elif parameter == "$Getrole":
-							name = ''
-							for i in range(len('$Getrole')+1,len(message.content)):
-								name += message.content[i]
-							member = message.guild.get_member_named(name)
-							if member == None:
+			if (len(message.content) > 0):
+				if message.content[0] == SYMBOL:
+					parameter = ''
+					for i in range(0,int(RANGE)):
+						parameter =parameter + message.content[i]
+						print (parameter)
+						if i == 3:
+							if message.author.guild_permissions.kick_members | message.author.guild_permissions.administrator:
+								if parameter == "$ban":
+									for z in range(len('$ban')+1,len(message.content)):
+										name = ''
+										if message.content[z] == '-':
+											symbol = message.content[z] + message.content[z+1]
+											if symbol == '-U':
+												for k in range(z+2 , len(message.content)):
+														if message.content[k] == ',':
+															z = count
+															break
+														elif message.content[k] == '"':
+															continue
+														else:
+															name = name + message.content[k]
+										banned = False
+										for memb in message.guild.members:
+											if memb.name == name:
+												banned = True
+												await message.channel.send(f"Banned {name}")
+												await message.guild.ban(memb)
+											else:
+												continue
+									break
+						if i == 4:
+							if parameter == "$ping":
+								guild = message.guild
+								await message.author.create_dm()
+								await message.author.dm_channel.send(f'boop {message.author.name}')
 								break
-							text = ''
-							for i in range(1,len(member.roles)):
-								text = member.roles[i].name + ''
-							await message.channel.send(f'{text}')
-							break 
-					if i == 10:
-						if parameter == '$Createrole':
-							if message.author.guild_permissions.manage_roles | message.author.guild_permissions.administrator:
-								Rolename = ''
-								Permission  = discord.Permissions(add_reactions = True, attach_files = True,change_nickname = True
-											 ,connect = True,create_instant_invite = True,embed_links = True,external_emojis = True,read_messages = True
-											 ,read_message_history = True,send_messages = True,speak  = True,stream = True,view_channel = True) 
-								Guild = message.guild
-								for z in range(len('$Createrole')+1,len(message.content)):
-									if message.content[z] == '-':
-										symbol = message.content[z] + message.content[z+1]
-										if symbol == '-N':
-											count = z + 2
-											for k in range(count , len(message.content)):
-												if message.content[k] == ' ':
-													z = count
-													break
-												elif message.content[k] == '"':
-													continue
-												else:
-													Rolename = Rolename + message.content[k]
-										elif symbol == '-A':
-											Permission = discord.Permissions.all()
-											z += 2
-										elif symbol == '-a':
-											Permission = discord.Permissions.advanced()
-											z+= 2
-										elif symbol == '-C':
-											Permission = discord.Permissions.all_channel()
-											z+= 2
-										elif symbol == '-G':
-											Permission = discord.Permissions.general()
-											z+= 2
-										elif symbol == '-M':
-											Permission = discord.Permissions.membership()
-											z+= 2
-										elif symbol == '-N':
-											Permission = discord.Permissions.none()
-											z+= 2
-										elif symbol == '-s':
-											Permission = discord.Permissions.stage()
-											z+= 2
-										elif symbol == '-S':
-											Permission = discord.Permissions.stage_moderator()
-											z+= 2
-										elif symbol == '-T':
-											Permission = discord.Permissions.text()
-											z+= 2
-										elif symbol == '-V':
-											Permission = discord.Permissions.voice()
-											z+= 2
-										else:
-											await message.channel.send("invalid command")
-								await Guild.create_role(name = Rolename,permissions = Permission)
+							elif parameter == "$help":
+								await message.channel.send('```Help Commands \n```')
+								break
+						if i == 5:
+							if parameter == "$hello":
+								await message.channel.send('Hello :eyes:')
+								break
+						if i == 7:
+							if parameter == "$Scalpel":
+								await message.channel.send("lub you doc..")
+								break
+							elif parameter == "$Getrole":
+								name = ''
+								for i in range(len('$Getrole')+1,len(message.content)):
+									name += message.content[i]
+								member = message.guild.get_member_named(name)
+								if member == None:
+									break
+								text = ''
+								for i in range(1,len(member.roles)):
+									text = member.roles[i].name + ''
+								await message.channel.send(f'{text}')
+								break 
+						if i == 10:
+							if parameter == '$Createrole':
+								if message.author.guild_permissions.manage_roles | message.author.guild_permissions.administrator:
+									Rolename = ''
+									Permission  = discord.Permissions(add_reactions = True, attach_files = True,change_nickname = True
+												 ,connect = True,create_instant_invite = True,embed_links = True,external_emojis = True,read_messages = True
+												 ,read_message_history = True,send_messages = True,speak  = True,stream = True,view_channel = True) 
+									Guild = message.guild
+									for z in range(len('$Createrole')+1,len(message.content)):
+										if message.content[z] == '-':
+											symbol = message.content[z] + message.content[z+1]
+											if symbol == '-N':
+												count = z + 2
+												for k in range(count , len(message.content)):
+													if message.content[k] == ',':
+														z = count
+														break
+													elif message.content[k] == '"':
+														continue
+													else:
+														Rolename = Rolename + message.content[k]
+											elif symbol == '-A':
+												Permission = discord.Permissions.all()
+												z += 2
+											elif symbol == '-a':
+												Permission = discord.Permissions.advanced()
+												z+= 2
+											elif symbol == '-C':
+												Permission = discord.Permissions.all_channel()
+												z+= 2
+											elif symbol == '-G':
+												Permission = discord.Permissions.general()
+												z+= 2
+											elif symbol == '-M':
+												Permission = discord.Permissions.membership()
+												z+= 2
+											elif symbol == '-N':
+												Permission = discord.Permissions.none()
+												z+= 2
+											elif symbol == '-s':
+												Permission = discord.Permissions.stage()
+												z+= 2
+											elif symbol == '-S':
+												Permission = discord.Permissions.stage_moderator()
+												z+= 2
+											elif symbol == '-T':
+												Permission = discord.Permissions.text()
+												z+= 2
+											elif symbol == '-V':
+												Permission = discord.Permissions.voice()
+												z+= 2
+											else:
+												await message.channel.send("invalid command")
+									await Guild.create_role(name = Rolename,permissions = Permission)
+								else:
+									await message.channel.send("Nice try :), but I thought ahead -June")
+								break
 							else:
-								await message.channel.send("Nice try :), but I thought ahead -June")
-							break
-						else:
-							await message.channel.send("ummm i don't know what to do")
-							break
+								await message.channel.send("ummm i don't know what to do")
+								break
 
 #Userroles - 9
 
